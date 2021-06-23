0594ecc6ac484fb3a6e7290e9748902c4c05e022
Yelena-McCarthy <y.getman83@gmail.com>
2021-06-23 15:18:40 +0100

    added annotation to bikeshare code

--- bikeshare_yg.py
+++ bikeshare_yg.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-
+#add city_data, months & days parameters
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york': 'new_york_city.csv',
               'washington': 'washington.csv' }
